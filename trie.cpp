f13a6b20c734a45f0ee75231ac6fbba5af06c9e6
Sandeep Satheesh <sansth4x496@gmail.com>
2022-04-17 23:48:52 +0530

    Refactor Trie implementation.

--- trie.cpp
+++ trie.cpp
@@ -1,139 +1,77 @@
-// following is an implementation of trie 
-// Code written to solve the problem https://www.codechef.com/problems/WSITES01 on codechef.com
-
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long 
-#define mp make_pair
-#define pb push_back
-#define sf scanf
-#define pf printf
 
-int flg=1; // flg == 0 if the answer does n't exist;
-string s; // used to fetch the input string; declared global as the trie function works on it directly instead of passing it as a paremeter
-char ch;
-
-struct node // each node of the trie would be of this type
-{
-	long cb,cnb,b,nb;
-	node* arr[26]; // each node would have atmost 26 edges; each for each character of the latin letters from 'a' to 'b';
-};
-
-vector <string > ans;
-long cnt=0;
-
-class trie
-{
+class Node {
 	public:
-	node * root; // root node of the trie
-	trie()
-	{root=NULL;}
-	
-	void ins(node *); // used to build the trie from the given set of string
-	void trav(node *,string); // used to traverse the string
-	void chek(node *); // perform the check function
+	bool word_flag;
+
+	Node* links[26];
+
+	Node() {
+		for (int pos = 0; pos < 26; pos++)
+			links[pos] = nullptr;
+		word_flag = false;
+	}
+
+	bool checkLink(char ch) {
+		return links[ch - 'a'];
+	}
+
+	void createLink(char ch) {
+		links[ch - 'a'] = new Node();
+	}
+
+	Node* getLink(char ch) {
+		return links[ch - 'a'];
+	}
 };
 
-void trie::chek(node *cnode)
-{
-	if(cnode!=NULL)
-	{
-		if(cnode->b>0 && cnode->cnb>0)
-			flg=0;
-	}
-	if(flg)
-	{
-		for(int i=0;i<26;i++)
-		{
-			if(cnode->arr[i]!=NULL)
-				chek(cnode->arr[i]);
-		}
-	}
-}
-void trie:: ins(node * cnode)
-{
-	if(cnt!=s.size())
-	{
-		if(root==NULL)
-		{
-			root = new node;
-			root->b=0;
-			root->cb=0;
-			root->nb=0;
-			root->cnb=0;
-			for(int i=0;i<26;i++)
-				root->arr[i]=NULL;
-			cnode=root;
-		}
-		int pos=s[cnt]-'a';
-		if(cnode->arr[pos]==NULL)
-		{
-			cnode->arr[pos]= new node;
-			cnode->arr[pos]->b=0;
-			cnode->arr[pos]->nb=0;
-			cnode->arr[pos]->cb=0;
-			cnode->arr[pos]->cnb=0;
-			for(int i=0;i<26;i++)
-				(cnode->arr[pos])->arr[i]=NULL;
-		}
-		cnt++;
-		ins(cnode->arr[pos]);
-		if(ch=='-')
-			cnode->cb++;
-		else
-			cnode->cnb++;
-	}
-	else
-	{
-		if(ch=='-')
-			cnode->b++;
-		else
-			cnode->nb++;
-	}
-	
-}
+class Trie {
+	private:
+	Node *root;
 
-void trie::trav(node * cnode,string s)
-{
-	if(cnode->nb==0 && cnode!=root && cnode->cnb==0)
-		ans.pb(s);
-	else
-	{
-		for(int i=0;i<26;i++)
-		{
-			string tmp=s;
-			tmp.pb('a'+i);
-			if(cnode->arr[i]!=NULL)
-				trav(cnode->arr[i],tmp);
+	void findAllWords(Node* node, string &cur_word) {
+		if (node->word_flag)
+			cout<<cur_word<<endl;
+
+		for (int pos = 0; pos < 26; pos++) {
+			if (node->links[pos]) {
+				cur_word.push_back('a' + pos);
+				findAllWords(node->links[pos], cur_word);
+				cur_word.pop_back();
+			}
 		}
 	}
-}
 
-int main()
-{
-	long n;
-	cin>>n;
-	
-	trie t;
-	for(long i=0;i<n;i++)
-	{
-		cin>>ch>>s;
-		cnt=0;
-		t.ins(t.root); // inserts each string as they come by...
+	public:
+	Trie() {
+		root = new Node();
 	}
-	string tmp="";
-	t.chek(t.root);
-	if(flg)
-	{
-		t.trav(t.root,tmp);
-		cout<<ans.size()<<endl;
-		//sort(ans.begin(),ans.end());
-		for(long i=0;i<ans.size();i++)
-			cout<<ans[i]<<endl;
+
+	void insertWord(string &s) {
+		auto node = root;
+
+		for (auto ch : s) {
+			if (!node->checkLink(ch))
+				node->createLink(ch);
+			node = node->getLink(ch);
+		}
+
+		node->word_flag = true;
 	}
-	else
-		cout<<-1<<endl;
+
+	void getAllWords() {
+		string word = "";
+		findAllWords(root, word);
+	}
+};
+
+int main() {
+	Trie *trie = new Trie();
+	vector<string> words = {"sandeep", "satheesh"}; //trie supprts only lower case letters between a-z.
+	for (auto word : words)
+		trie->insertWord(word);
+	trie->getAllWords();
 	return 0;
 }
